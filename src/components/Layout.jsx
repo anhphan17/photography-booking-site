4798cac40006b1677ce561d6914bdd5aab328caf
anhphan <anhnguyenphan03@gmail.com>
2025-09-18 16:01:37 -0400

    Footer component added.

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
+import './Layout.css';
 import { Outlet } from "react-router-dom";
 
-function Layout({children}) {
+function Layout() {
     return (
-        <div>
+        <div className="layout">
             <Navbar />
-            <main>
+            <main className="main-content">
                 <Outlet />
             </main>
+            <Footer />
         </div>
     );
 }
